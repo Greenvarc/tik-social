c75ec99b43651e0cb4a21fef633c49b92a21e0d8
robert varchodi <rvarchodi@gmail.com>
2022-12-27 15:56:47 +0300

    change cfg

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["images8.alphacoders.com", "lh3.googleusercontent.com"],
